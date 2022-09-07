412fdecc57c02cd7a73132dfcde87b0128a510a5
timirpaul <89598473+timirpaul@users.noreply.github.com>
2022-09-08 00:53:09 +0530

    add env data

--- db/db.js
+++ db/db.js
@@ -9,7 +9,7 @@ const dbPass = process.env.DB_PASSWORD
 
 
   //  const url = `mongodb://localhost:27017/new`
-    const url = `mongodb+srv://${dbUser}:${dbPass}@cluster0.xyb9h.mongodb.net/Blogdb?retryWrites=true&w=majority`
+    const url = `mongodb+srv://Blogdb:Blog123@cluster0.xyb9h.mongodb.net/Blogdb?retryWrites=true&w=majority`
     try {
         mongoose.connect(url,{useNewUrlParser: true , useUnifiedTopology: true})
       console.log("database connection succesfully");
