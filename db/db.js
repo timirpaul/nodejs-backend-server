6260fa8b5ebc50e83e0ee9d6b83559cf3cd3ccb2
timirpaul <89598473+timirpaul@users.noreply.github.com>
2022-09-08 01:06:19 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,15 +1,15 @@
 
 const mongoose = require("mongoose")
-//const env = require("dotenv")
+const env = require("dotenv")
 
-//env.config();
+env.config();
 
-//const dbUser = process.env.DB_USERNAME  
-//const dbPass = process.env.DB_PASSWORD 
+const dbUser = process.env.DB_USERNAME  
+const dbPass = process.env.DB_PASSWORD 
 
 
   //  const url = `mongodb://localhost:27017/new`
-    const url = `mongodb+srv://Blogdb:Blog123@cluster0.xyb9h.mongodb.net/Blogdb?retryWrites=true&w=majority`
+    const url = `mongodb+srv://${dbUser}:${dbPass}@cluster0.xyb9h.mongodb.net/Blogdb?retryWrites=true&w=majority`
     try {
         mongoose.connect(url,{useNewUrlParser: true , useUnifiedTopology: true})
       console.log("database connection succesfully");
